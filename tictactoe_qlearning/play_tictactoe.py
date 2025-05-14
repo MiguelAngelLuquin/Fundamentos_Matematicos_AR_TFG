5695e4546431f95cd795ee0510098bbe8ba9a551
Miguel Angel Luquín Guerrero <mluquinguerrero@gmail.com>
2025-05-14 21:50:09 +0200

    Add files via upload

---
+++ tictactoe_qlearning/play_tictactoe.py
@@ -0,0 +1,253 @@
+#################################################################################
+# Autor: Miguel Ángel Luquín Guerrero
+# Fecha de creación: 23/01/2025
+# Última modificación: 25/01/2025
+# Versión: 1.0
+# Descripción: Programa que permite jugar al 3 en raya contra la máquina
+#              que ha sido entrenada con el algoritmo Q-Learning. Así como probar a
+#              la máquina contra un algoritmo aleatorio.
+#              Para ello, se debe cargar la tabla Q desde el archivo tictactoe.csv
+#              generado por el programa tictactoe_train.py
+#################################################################################
+
+import csv
+import numpy as np
+import random
+
+# Parámetros del entorno
+ROWS = 3
+COLS = 3
+
+# Leer la matriz desde el archivo CSV
+def cargar_tabla():
+    with open('tictactoe.csv', mode='r') as archivo:
+        lector = csv.reader(archivo)
+        q_table = [list(map(float, fila)) for fila in lector]
+    return q_table
+
+####Funciones auxiliares####
+def tablero_inicial():
+    tab = np.zeros((ROWS, COLS))
+    return tab
+
+def next_tab(tab, action, player):
+    tab[((action-1)//COLS)][(action-1)%COLS] = player
+    return tab
+
+def next_states(state, action, player):
+    state += player*3**(9-action)
+    return state
+
+def checkresult(tab):
+    ganador = 0
+    for i in range(3):
+        if tab[i][0] == tab[i][1] == tab[i][2] != 0:
+            ganador = tab[i][0]
+        if tab[0][i] == tab[1][i] == tab[2][i] != 0:
+            ganador = tab[0][i]
+    if tab[0][0] == tab[1][1] == tab[2][2] != 0:
+        ganador = tab[0][0]
+    if tab[0][2] == tab[1][1] == tab[2][0] != 0:
+        ganador = tab[0][2]
+    return ganador
+############################
+
+
+# Función para imprimir el tablero
+def imprimir_rejilla(numero):
+    numero = convertir_base(numero,10,3)
+    numero_str = str(numero).zfill(9)
+    casillas = [int(digito) for digito in numero_str]
+
+    # Imprimir la rejilla de 3x3
+    for i in range(0, 9, 3):
+        fila = casillas[i:i+3]  # Extraemos cada fila de 3 casillas
+        fila_mostrada = [' ' if x == 0 else 'X' if x == 1 else 'O' for x in fila]
+        print(" | ".join(fila_mostrada))
+        if i < 6:  # Agregar una línea separadora entre las filas
+            print("---------")
+    print(" ")
+
+# Funcion para cambiar de base n a base m
+def convertir_base(num, base_n, base_m):
+    num_decimal = 0
+    potencia = 0
+    while num > 0:
+        digito = num % 10
+        num_decimal += digito * (base_n ** potencia)
+        num //= 10
+        potencia += 1
+    
+    if num_decimal == 0:
+        return 0
+    
+    resultado = 0
+    multiplicador = 1
+    while num_decimal > 0:
+        resultado += (num_decimal % base_m) * multiplicador
+        num_decimal //= base_m
+        multiplicador *= 10
+    
+    return resultado
+
+# Función par aprobar el algoritmo Q-Learning contra uno aleatorio  
+# jugando aleatoriamente como jugador 1 o 2
+def jugar_contra_random(q_table):
+    
+    # Contadores de victorias
+    win1 = 0
+    win2 = 0
+    GAMES = 100000
+
+    print("Iniciando juegos: 0.0%")
+    for i in range(GAMES):
+        if (i+1)%10000 == 0:
+            print("Jugado un", (i+1)*100/GAMES, "%")
+
+        # Elejigmos el turno de la máquina aleatoriamente
+        jugada = 1
+
+        # Inicializamos el estado, el tablero y el turno
+        ACTIONS = [1,2,3,4,5,6,7,8,9]
+        tablero = tablero_inicial()
+        turn = 0
+        state = 0
+
+        # Esto representa una jugada de cada uno
+        while True: 
+            
+            turn += 1
+
+            #Jugada del algoritmo Q-Learning cuando le toque
+            if (turn%2 == 1 and jugada == 1):
+                maxim = q_table[state][ACTIONS[0]-1]
+                action = ACTIONS[0]
+                for ac in range(len(ACTIONS)-1):
+                    if q_table[state][ACTIONS[ac+1]-1] > maxim:
+                        maxim = q_table[state][ACTIONS[ac+1]-1]
+                        action = ACTIONS[ac+1]
+                next_state = next_states(state, action, 1)
+                tablero = next_tab(tablero, action, 1)
+            elif (turn%2 == 0 and jugada == 2):
+                minim = q_table[state][ACTIONS[0]-1]
+                action = ACTIONS[0]
+                for ac in range(len(ACTIONS)-1):
+                    if q_table[state][ACTIONS[ac+1]-1] < minim:
+                        minim = q_table[state][ACTIONS[ac+1]-1]
+                        action = ACTIONS[ac+1]
+                next_state = next_states(state, action, 1)
+                tablero = next_tab(tablero, action, 1)
+            #Jugada aleatoria
+            else:
+                action = random.choice(ACTIONS)
+                next_state = next_states(state, action, 2)
+                tablero = next_tab(tablero, action, -1)
+
+            # Quitamos la casilla ya ocupada
+            ACTIONS.remove(action) 
+
+            # Comprobamos si hay ganador y gestionamos el marcador
+            fin = checkresult(tablero) 
+            if fin > 0:
+                win1 += 1
+                break
+            elif fin < 0:
+                win2 += 1
+                break
+            elif turn == 9:
+                break
+
+            state = next_state
+
+    # Han terminado las partidas, mostramos los resultados
+    print("Victorias Jugador 1: ", win1)
+    print("Empates: ", GAMES-win1-win2)
+    print("Victorias Jugador 2: ", win2)
+
+def jugar_contra_maquina(q_table):
+    # Inicializamos el tablero y el estado
+    ACTIONS = [1,2,3,4,5,6,7,8,9]
+    tablero = tablero_inicial()
+    state = 0
+    turn = 0
+
+    # El usuario elige ser el jugador 1 o 2
+    jugada = "0"
+    while jugada != "1" and jugada != "2":
+        jugada = input("¿Quieres ser el jugador 1 o el jugador 2? (1/2): ")
+    #Bucle de juego
+    while True:
+        imprimir_rejilla(state)
+
+        turn += 1
+
+        if (turn%2 == 1 and jugada == "2"):
+            print("Turno de la máquina")
+            maxim = q_table[state][ACTIONS[0]-1]
+            action = ACTIONS[0]
+            for ac in range(len(ACTIONS)-1):
+                if q_table[state][ACTIONS[ac+1]-1] > maxim:
+                    maxim = q_table[state][ACTIONS[ac+1]-1]
+                    action = ACTIONS[ac+1]
+            next_state = next_states(state, action, 2)
+            tablero = next_tab(tablero, action, -1)
+
+        elif(turn%2 == 0 and jugada == "1"):
+            print("Turno de la máquina")
+            minim = q_table[state][ACTIONS[0]-1]
+            action = ACTIONS[0]
+            for ac in range(len(ACTIONS)-1):
+                if q_table[state][ACTIONS[ac+1]-1] < minim:
+                    minim = q_table[state][ACTIONS[ac+1]-1]
+                    action = ACTIONS[ac+1]
+            next_state = next_states(state, action, 2)
+            tablero = next_tab(tablero, action, -1)
+
+        else:
+            print("Introduce la casilla en la que quieres jugar: (1 = arriba izqda, 2 arriba centro,... , 9 abajo derecha)")
+            casilla = int(input("--> "))
+            action = int(casilla)
+            while action not in ACTIONS:
+                print("Casilla incorrecta u ocupada")
+                print("Introduce la casilla en la que quieres jugar: (1 = arriba izqda, 2 arriba centro,... , 9 abajo derecha)")
+                casilla = int(input("--> "))
+                action = int(casilla)
+                continue
+            next_state = next_states(state, action, 1)
+            tablero = next_tab(tablero, action, 1)
+        
+        # Quitamos la casilla ya ocupada y actualizamos el estado
+        ACTIONS.remove(action)
+        state = next_state
+
+        # Comprobamos si hay ganador y gestionamos el marcador
+        fin = checkresult(tablero) 
+        if fin > 0:
+            imprimir_rejilla(state)
+            print("¡Has ganado!")
+            break
+        elif fin < 0:
+            imprimir_rejilla(state)
+            print("¡Has perdido!")
+            break
+        elif turn == 9:
+            imprimir_rejilla(state)
+            print("¡Empate!")
+            break
+
+#Función principal
+q_table = cargar_tabla()
+terminate = False
+while not terminate:
+    print("¿Qué desea hacer? (Introduce el número de la opción)")
+    print("1. Jugar contra la máquina")
+    print("2. Probar la máquina contra un algoritmo aleatorio")
+    print("3. Salir")
+    opcion = input("--> ")
+    if opcion == "1":
+        jugar_contra_maquina(q_table)
+    elif opcion == "2":
+        jugar_contra_random(q_table)
+    elif opcion == "3":
+        print("Saliendo del programa...")
+        terminate = True
---
+++ tictactoe_qlearning/tictactoe_train.py
@@ -0,0 +1,191 @@
+#################################################################################
+# Autor: Miguel Ángel Luquín Guerrero
+# Fecha de creación: 23/01/2025
+# Última modificación: 25/01/2025
+# Versión: 3.0
+# Descripción: Programa que entrena a la máquina a jugar al 3 en raya
+#              usando el algoritmo Q-Learning contra sí mismo. En esta 
+#              versión se ha añadido la posibilidad de guardar la tabla 
+#              Q en un .csv para poder usarla en el programa play_tictactoe.py
+#################################################################################
+
+import numpy as np
+import random
+import csv
+
+# Parámetros del entorno
+ROWS = 3
+COLS = 3
+STATES = 3**(ROWS*COLS)
+EPISODES = 100000 #100 000 iteraciones
+INI = 0
+
+# Crear el tablero inicial
+def tablero_inicial():
+    tab = np.zeros((ROWS, COLS))
+    return tab
+
+# Obtener la siguiente jugada dados el estado y la acción
+def next_tab(tab, action, player):
+    tab[((action-1)//COLS)][(action-1)%COLS] = player
+    return tab
+
+# Definimos el siguiente estado
+def next_states(state, action, player):
+    state += player*3**(9-action)
+    return state
+
+#Comprobamos si alguien ha ganado o si la partida ha terminado en empate
+def checkresult(tab):
+    ganador = 0
+    for i in range(3):
+        if tab[i][0] == tab[i][1] == tab[i][2] != 0:
+            ganador = tab[i][0]
+        if tab[0][i] == tab[1][i] == tab[2][i] != 0:
+            ganador = tab[0][i]
+    if tab[0][0] == tab[1][1] == tab[2][2] != 0:
+        ganador = tab[0][0]
+    if tab[0][2] == tab[1][1] == tab[2][0] != 0:
+        ganador = tab[0][2]
+    return ganador
+
+# Algoritmo Q-Learning para entrenar a la máquina a jugar en ambos roles
+def q_learning(episodes=EPISODES, alpha=0.1, gamma=0.95):
+    q_table = np.zeros((STATES, ROWS*COLS))  # Inicializar tabla Q
+
+    #Contadores de victorias, empates y derrotas (con respecto del jugador 1)
+    wins = 0
+    draws = 0
+    losses = 0
+
+    #numero de episodios
+    for episode in range(episodes):
+        if episode%5000 == 0:
+            print(episode*100/episodes, "%")
+
+        #inicializamos el estado, el tablero y el turno
+        state = INI
+        tablero = tablero_inicial()
+        turn = 0
+        reward = 0
+
+        # Factor de exploración que va decreciendo con los episodios
+        epsilon = max(0.2, 1 - episode/episodes) 
+
+        # En el principio de cada episodio, el agente comienza con todas las posibilidades de acción
+        ACTIONS = [1,2,3,4,5,6,7,8,9]
+
+        # Esto representa una jugada de cada uno
+        while True:
+            
+            turn += 1
+    
+            ##########Jugador 1##########
+
+            # Decidir la acción (explorar/explotar)
+            if random.uniform(0, 1) < epsilon:
+                action = random.choice(ACTIONS)
+            else:
+                maxim = q_table[state][ACTIONS[0]-1]
+                action = ACTIONS[0]
+                for ac in range(len(ACTIONS)-1):
+                    if q_table[state][ACTIONS[ac+1]-1] > maxim:
+                        maxim = q_table[state][ACTIONS[ac+1]-1]
+                        action = ACTIONS[ac+1]
+
+            # Actualizar el estado, el tablero y quitamos la acción de las posibles
+            next_state = next_states(state, action, 1)
+            tablero = next_tab(tablero, action, 1)
+            ACTIONS.remove(action)
+
+            # Comprobamos si hay ganador
+            reward = checkresult(tablero) 
+
+            # Actualizar la tabla Q de la jugada anterior
+            if len(ACTIONS) == 0:
+                best_next_action = 0
+            else:
+                valid_q_values = [q_table[next_state][a-1] for a in ACTIONS]
+                best_next_action = np.min(valid_q_values)
+            q_table[state][action-1] += alpha * (
+                reward + gamma * best_next_action - q_table[state][action-1]
+            )
+
+            if reward > 0:
+                wins += 1
+                break
+            elif turn == 9:
+                draws += 1
+                break
+
+            # Fin turno Jugador 1
+            state = next_state
+            turn += 1
+
+            ##########Jugador 2##########
+
+            # Decidir la acción (explorar/explotar)
+            if random.uniform(0, 1) < epsilon:
+                action = random.choice(ACTIONS)
+            else:
+                minim = q_table[state][ACTIONS[0]-1]
+                action = ACTIONS[0]
+                for ac in range(len(ACTIONS)-1):
+                    if q_table[state][ACTIONS[ac+1]-1] < minim:
+                        minim = q_table[state][ACTIONS[ac+1]-1]
+                        action = ACTIONS[ac+1]
+
+            # Actualizar el estado, el tablero y quitamos la acción de las posibles
+            next_state = next_states(state, action, 2)
+            tablero = next_tab(tablero, action, -1)
+            ACTIONS.remove(action) # Quitamos la casilla ya ocupada
+
+            # Comprobamos si hay ganador
+            reward = checkresult(tablero) # Comprobamos si hay ganador
+            
+            # Actualizar la tabla Q de la jugada anterior
+            valid_q_values = [q_table[next_state][a-1] for a in ACTIONS]
+            best_next_action = np.max(valid_q_values)
+            q_table[state][action-1] += alpha * (
+                reward + gamma * best_next_action - q_table[state][action-1]
+            )
+
+            if reward < 0:
+                losses += 1
+                break
+
+            state = next_state
+
+    return q_table, wins, draws, losses
+
+# Función que muestra el ratio de victorias de Jug 1 y 2
+def resultados(wins, draws, loses):
+    print("Player 1: ", wins*100/EPISODES, "%")
+    print("Draws: ", draws*100/EPISODES, "%")
+    print("Player 2: ", loses*100/EPISODES, "%")   
+
+#Función que muestra cuántos estados ha enfrentado la máquina
+def mirar_estados(tabla):
+    count = 0
+    for state in range(STATES):
+        if not all(x == 0 for x in tabla[state]):
+            count += 1
+    print("Se ha enfrentado la maquina a ", count, " estados.")
+
+#Función que guarda la tabla Q en un .csv
+def guardar_tabla(tabla):
+    with open('tictactoe.csv', mode='w', newline='') as archivo:
+        escritor = csv.writer(archivo)
+        escritor.writerows(tabla)
+
+
+#---INICIO DE PROGRAMA---#
+
+#Ejecución del algoritmo Q-Learning
+q_table, wins, draws, loses = q_learning()
+
+#Mostrar resultados
+resultados(wins, draws, loses)
+mirar_estados(q_table)
+#Guardar tabla Q en un .csv
+guardar_tabla(q_table)
